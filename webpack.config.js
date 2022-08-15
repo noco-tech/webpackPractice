bdf95cfb522d513b125e3a0890a8a9e6d1829b08
sadainoaraisan <73414778+sadainoaraisan@users.noreply.github.com>
2022-08-15 15:25:19 +0900

    es6_source-map_image-webpack-loader-_Add hash to filename

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,27 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  mode: "devlopment",
+  devtool: "source-map",
   entry: "./src/javascripts/main.js",
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename: "./javascripts/main.js",
+    filename: "./javascripts/[name]-[contenthash]].js",
   },
   module: {
     rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+      },
       {
         test: /\.css/,
         use: [
@@ -19,14 +33,23 @@ module.exports = {
           },
           {
             loader: "css-loader",
+            // options: { sourceMap: true },
           },
         ],
       },
       {
-        test: /\.(png|jpg|jpeg|gif)/,
+        test: /\.(png|jpg|jpeg|gif|webp)/,
         type: "asset/resource",
         generator: {
-          filename: "images/[name][ext]",
+          filename: "images/[name]-[contenthash][ext]",
+        },
+
+        loader: "image-webpack-loader",
+        options: {
+          mozjpeg: {
+            progressive: true,
+            quality: 65,
+          },
         },
         // use: [
         //   {
@@ -42,10 +65,15 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "./stylesheets/main.css",
+      filename: "./stylesheets/[name]-[contenthash].css",
     }),
     new HtmlWebpackPlugin({
       template: "./src/templates/index.html",
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/access.html",
+      filename: "access.html",
     }),
     new CleanWebpackPlugin(),
   ],
